dc2310c35d8d6ac337a1ab1cb5463c173dbf0205
NEXT876 <waibel.ben@gmail.com>
2024-12-03 16:12:59 +0100

    Create Red_Nosed_Reports.java

---
+++ day_2/Red_Nosed_Reports.java
@@ -0,0 +1,13 @@
+// Red_Nosed_Reports.java
+
+
+import java.util.Scanner;
+
+public class Red_Nosed_Reports {
+
+	Scanner scanner = new Scanner(System.in);
+
+	public static void main(String[] args) {
+
+	}
+}
