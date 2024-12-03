5aeefd67cdc4156855478fa488a22a4330246c79
NEXT876 <waibel.ben@gmail.com>
2024-12-03 12:42:24 +0100

    sjdjsd

---
+++ day_1/Historian_Hysteria.java
@@ -0,0 +1,32 @@
+//Historian_Hysteria.java
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Historian_Hysteria {
+
+    private static final Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        ArrayList<Integer> liste1 = new ArrayList<>();
+        ArrayList<Integer> liste2 = new ArrayList<>();
+
+        
+
+        while (scanner.hasNext()) {
+           
+            liste1.add(scanner.nextInt());
+            //scanner.nextLine();
+            liste2.add(scanner.nextInt());
+            //scanner.nextLine();
+            
+            
+        }
+        
+       // System.out.println(liste1);
+       // System.out.println(liste2);
+       // System.out.println("ende");
+
+        
+    }
+}
