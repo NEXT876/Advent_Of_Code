5acf2dae726c271b39f6d9882d78a21be2a39cc8
NEXT876 <waibel.ben@gmail.com>
2024-12-03 13:46:23 +0100

    day_1

--- day_1/Historian_Hysteria.java
+++ day_1/Historian_Hysteria.java
@@ -1,32 +1,54 @@
 //Historian_Hysteria.java
 
+
 import java.util.ArrayList;
 import java.util.Scanner;
-
-public class Historian_Hysteria {
+import java.util.Collections;
+/** erstes Rätsel.*/
+ public class Historian_Hysteria {
 
     private static final Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args) {
 
         ArrayList<Integer> liste1 = new ArrayList<>();
         ArrayList<Integer> liste2 = new ArrayList<>();
 
-        
 
         while (scanner.hasNext()) {
-           
             liste1.add(scanner.nextInt());
-            //scanner.nextLine();
             liste2.add(scanner.nextInt());
-            //scanner.nextLine();
-            
-            
         }
-        
-       // System.out.println(liste1);
-       // System.out.println(liste2);
-       // System.out.println("ende");
 
-        
+        int distance = 0;
+
+        bubbleSort(liste1);
+        bubbleSort(liste2);
+
+        for (int i = 0; i < liste1.size(); i++) {
+            distance += Math.abs(liste1.get(i) - liste2.get(i));
+        }
+
+        System.out.println(distance);
+
+
     }
+
+    public static void bubbleSort(ArrayList<Integer> list) {
+        int n = list.size();
+        for (int i = 0; i < n - 1; i++) {
+            for (int j = 0; j < n - i - 1; j++) {
+                if (list.get(j) > list.get(j + 1)) {
+                    // Tauschen mit Collections.swap()
+                    Collections.swap(list, j, j + 1);
+                }
+            }
+        }
+    }
+
 }
+
+
+
+
+
---
+++ day_1/Part_Two.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Part_Two {
+	private static final Scanner scanner = new Scanner(System.in);
+
+	public static void main(String[] args) {
+
+		//ArrayList<Integer>
+		ArrayList<Integer> liste1, liste2;
+		liste1 = new ArrayList<>();
+		liste2 = new ArrayList<>();
+
+		while (scanner.hasNext()) {
+			liste1.add(scanner.nextInt());
+			liste2.add(scanner.nextInt());
+		}
+
+		int similarity = 0;
+		for (int i = 0; i < liste1.size(); ++i) {
+			int anzahl = 0;
+			for (Object item2 : liste2) { // Iterate over liste2 directly
+				if (liste1.get(i).equals(item2)) {
+					anzahl++;
+				}
+			}
+			similarity += anzahl * liste1.get(i);
+		}
+		System.out.println(similarity);
+
+
+	}
+}
