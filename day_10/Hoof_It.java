fc5cf8ef7d35a956e60311ebde3b646621e8a462
NEXT876 <waibel.ben@gmail.com>
2024-12-12 18:53:07 +0100

    day_10/unfinished

--- day_10/Hoof_It.java
+++ day_10/Hoof_It.java
@@ -3,89 +3,39 @@
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.sql.SQLOutput;
 import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Hoof_It {
 
+
+    //public static int n = 1;
+    public static int jj;
+    public static int ii;
+
     public static void main(String[] args) {
 
-        ArrayList<String> InputZeilen = new ArrayList<>();
+        ArrayList<String> InputZeilen = Inputverarbeiten();
 
-
-        try {
-            File file = new File("day_10/input.txt");
-            Scanner scanner = new Scanner(file);
-
-            while (scanner.hasNextLine()) {
-                InputZeilen.add(scanner.nextLine());
-            }
-            for (int i = 0; i < InputZeilen.size(); i++) {
-                System.out.println(InputZeilen.get(i));
-            }
-            System.out.println();
-            scanner.close();
-        } catch (FileNotFoundException e) {
-            System.out.println("Die Datei konnte nicht gefunden werden: " + e.getMessage());
-            return;
-        }
 
         for (int i = 0; i < InputZeilen.size(); i++) {
             String aktuelleZeile = InputZeilen.get(i);
             for (int j = 0; j < aktuelleZeile.length(); j++) {
                 if (aktuelleZeile.charAt(j) == '0') {
 
-                    // prüfe nach rechts
                     int n = 0;
-                    int jj = j;
-                    if (jj + 1 < aktuelleZeile.length() && aktuelleZeile.charAt(j) - '0' + 1 == aktuelleZeile.charAt(jj + 1) - '0') {
-                        System.out.print("nach rechts: ");
-                        while (jj < aktuelleZeile.length() && (aktuelleZeile.charAt(j) - '0') + n == aktuelleZeile.charAt(jj) - '0') {
-                            System.out.print(aktuelleZeile.charAt(jj));
-                            n++;
-                            jj = j + n;
-                        }
-                        System.out.println();
-                    }
-                    // prüfe nach links
-                    n = 0;
-                    jj = j;
-                    if (jj - 1 >= 0 && aktuelleZeile.charAt(j) - '0' + 1 == aktuelleZeile.charAt(jj - 1) - '0') {
-                        System.out.print("nach links: ");
-                        while (jj >= 0 && (aktuelleZeile.charAt(j) - '0') + n == aktuelleZeile.charAt(jj) - '0') {
-                            System.out.print(aktuelleZeile.charAt(jj));
-                            n++;
-                            jj = j - n;
-                        }
-                        System.out.println();
-                    }
-                    // prüfe nach unten
-                    n = 0;
-                    int ii = i;
-                    if (ii + 1 < InputZeilen.size() && InputZeilen.get(i).charAt(j) - '0' + 1 == InputZeilen.get(ii + 1).charAt(j) - '0') {
-                        System.out.print("nach unten: ");
-                        while (ii < InputZeilen.size() && InputZeilen.get(i).charAt(j) - '0' + n == InputZeilen.get(ii).charAt(j) - '0') {
-                            System.out.print(InputZeilen.get(ii).charAt(j));
-                            n++;
-                            ii = i + n;
-                        }
-                        System.out.println();
-                    }
-                    // prüfe nach oben
-                    n = 0;
-                    ii = i;
-                    if (ii - 1 >= 0 && InputZeilen.get(i).charAt(j) - '0' + 1 == InputZeilen.get(ii - 1).charAt(j) - '0') {
-                        System.out.print("nach oben: ");
-                        while (ii - 1 >= 0 && InputZeilen.get(i).charAt(j) - '0' + n == InputZeilen.get(ii).charAt(j) - '0') {
-                            System.out.print(InputZeilen.get(ii).charAt(j));
-                            n++;
-                            ii = i - n;
-                        }
-                        System.out.println();
-                    }
+                    pruefeAufRoutenRechts(aktuelleZeile, InputZeilen, j, i, n);
 
-                    //System.out.println(aktuelleZeile);
+                    n = 0;
+                    pruefeAufRoutenLinks(aktuelleZeile, InputZeilen, j, i, n);
+
+                    n = 0;
+                    pruefeAufRoutenUnten(aktuelleZeile, InputZeilen, j, i, n);
+
+                    n = 0;
+                    pruefeAufRoutenOben(aktuelleZeile, InputZeilen, j, i, n);
+
+
                 }
             }
 
@@ -94,4 +44,181 @@ public class Hoof_It {
 
 
     }
+
+    private static ArrayList<String> Inputverarbeiten() {
+        ArrayList<String> inputZeilen = new ArrayList<>();
+        try {
+            File file = new File("day_10/input.txt");
+            Scanner scanner = new Scanner(file);
+
+            while (scanner.hasNextLine()) {
+                inputZeilen.add(scanner.nextLine());
+            }
+            for (int i = 0; i < inputZeilen.size(); i++) {
+                System.out.println(inputZeilen.get(i));
+            }
+            System.out.println();
+            scanner.close();
+        } catch (FileNotFoundException e) {
+            System.out.println("Die Datei konnte nicht gefunden werden: " + e.getMessage());
+        }
+        return inputZeilen;
+    }
+
+
+    private static void pruefeAufRoutenRechts(String aktuelleZeile, ArrayList<String> InputZeilen, int j, int i, int n) {
+        // prüfe nach rechts
+        if (j + 1 < aktuelleZeile.length() && 1 == aktuelleZeile.charAt(j + 1) - '0') {
+            System.out.print("nach rechts: ");
+            while (j < aktuelleZeile.length() && n == aktuelleZeile.charAt(j) - '0') {
+                System.out.print(aktuelleZeile.charAt(j));
+                n++;
+                j++;
+            }
+            j--;
+            pruefeAufWeitereRouten(aktuelleZeile, InputZeilen, j, i, n, "Unten");
+            pruefeAufWeitereRouten(aktuelleZeile, InputZeilen, j, i, n, "Oben");
+
+            System.out.println();
+        }
+    }
+
+    private static void pruefeAufRoutenLinks(String aktuelleZeile, ArrayList<String> InputZeilen, int j, int i, int n) {
+
+        // prüfe nach links
+        if (j - 1 >= 0 && 1 == aktuelleZeile.charAt(j - 1) - '0') {
+            System.out.print("nach links: ");
+            while (j >= 0 && n == aktuelleZeile.charAt(j) - '0') {
+                System.out.print(aktuelleZeile.charAt(j));
+                n++;
+                j--;
+            }
+            j++;
+            pruefeAufWeitereRouten(aktuelleZeile, InputZeilen, j, i, n, "Unten");
+            pruefeAufWeitereRouten(aktuelleZeile, InputZeilen, j, i, n, "Oben");
+
+            System.out.println();
+
+        }
+    }
+
+    private static void pruefeAufRoutenUnten(String aktuelleZeile, ArrayList<String> InputZeilen, int j, int i, int n) {
+
+
+        // prüfe nach unten
+        if (i + 1 < InputZeilen.size() && 1 == InputZeilen.get(i + 1).charAt(j) - '0') {
+            System.out.print("Nach unten: ");
+            while (i < InputZeilen.size() && n == InputZeilen.get(i).charAt(j) - '0') {
+                System.out.print(InputZeilen.get(i).charAt(j));
+                n++;
+                i++;
+            }
+
+            i--;
+            aktuelleZeile = InputZeilen.get(i);
+            pruefeAufWeitereRouten(aktuelleZeile, InputZeilen, j, i, n, "Rechts");
+            pruefeAufWeitereRouten(aktuelleZeile, InputZeilen, j, i, n, "Links");
+
+            System.out.println();
+        }
+    }
+
+    private static void pruefeAufRoutenOben(String aktuelleZeile, ArrayList<String> InputZeilen, int j, int i, int n) {
+
+
+        // prüfe nach oben
+        if (i - 1 >= 0 && 1 == InputZeilen.get(i - 1).charAt(j) - '0') {
+            System.out.print("nach oben: ");
+            while (i >= 0 && n == InputZeilen.get(i).charAt(j) - '0') {
+                System.out.print(InputZeilen.get(i).charAt(j));
+                n++;
+                i--;
+            }
+            i++;
+            aktuelleZeile = InputZeilen.get(i);
+            pruefeAufWeitereRouten(aktuelleZeile, InputZeilen, j, i, n, "Links");
+            pruefeAufWeitereRouten(aktuelleZeile, InputZeilen, j, i, n, "Rechts");
+
+            System.out.println();
+        }
+    }
+
+    private static void pruefeAufWeitereRouten(String aktuelleZeile, ArrayList<String> InputZeilen, int j, int i, int n, String direction) {
+
+
+        switch (direction) {
+            case "Rechts":
+                if (j + 1 < aktuelleZeile.length() && n == aktuelleZeile.charAt(j + 1) - '0') {
+                    j++;
+                    while (j < aktuelleZeile.length() && n == aktuelleZeile.charAt(j) - '0') {
+                        if (j < aktuelleZeile.length()) {
+                            System.out.print(aktuelleZeile.charAt(j));
+                        }
+                        n++;
+                        j++;
+
+                    }
+                    j--;
+                    pruefeAufWeitereRouten(aktuelleZeile, InputZeilen, j, i, n, "Unten");
+                    pruefeAufWeitereRouten(aktuelleZeile, InputZeilen, j, i, n, "Oben");
+
+                }
+
+                break;
+
+            case "Links":
+                if (j - 1 >= 0 && n == aktuelleZeile.charAt(j - 1) - '0') {
+                    j--;
+                    while (j >= 0 && n == aktuelleZeile.charAt(j) - '0') {
+                        if (j >= 0) {
+                            System.out.print(aktuelleZeile.charAt(j));
+                        }
+                        n++;
+                        j--;
+
+                    }
+                    j++;
+                    pruefeAufWeitereRouten(aktuelleZeile, InputZeilen, j, i, n, "Oben");
+                    pruefeAufWeitereRouten(aktuelleZeile, InputZeilen, j, i, n, "Unten");
+                }
+                break;
+
+            case "Oben":
+                if (i - 1 >= 0 && j < aktuelleZeile.length() && n == InputZeilen.get(i - 1).charAt(j) - '0') {
+                    i--;
+                    while (i >= 0 && j < InputZeilen.get(i).length() && n == InputZeilen.get(i).charAt(j) - '0') {
+                        if (i >= 0 && j < InputZeilen.get(i).length()) {
+                            System.out.print(InputZeilen.get(i).charAt(j));
+                        }
+                        n++;
+                        i--;
+
+                    }
+                    i++;
+                    aktuelleZeile = InputZeilen.get(i);
+                    pruefeAufWeitereRouten(aktuelleZeile, InputZeilen, j, i, n, "Links");
+                    pruefeAufWeitereRouten(aktuelleZeile, InputZeilen, j, i, n, "Rechts");
+                }
+                break;
+
+            case "Unten":
+                if (i + 1 < InputZeilen.size() && j < aktuelleZeile.length() && n == InputZeilen.get(i + 1).charAt(j) - '0') {
+                    i++;
+                    while (i < InputZeilen.size() && j < InputZeilen.get(i).length() && n == InputZeilen.get(i).charAt(j) - '0') {
+                        if (i < InputZeilen.size() && j < InputZeilen.get(i).length()) {
+                            System.out.print(InputZeilen.get(i).charAt(j));
+                        }
+                        n++;
+                        i++;
+                    }
+                    i--;
+                    aktuelleZeile = InputZeilen.get(i);
+                    pruefeAufWeitereRouten(aktuelleZeile, InputZeilen, j, i, n, "Rechts");
+                    pruefeAufWeitereRouten(aktuelleZeile, InputZeilen, j, i, n, "Links");
+                }
+                break;
+
+        }
+    }
+
 }
